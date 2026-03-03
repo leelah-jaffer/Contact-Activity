75c6742d2eb77c58f7ea7f28f23b57ae00e0ab8c
leelah-jaffer <jafferleelah6@gmail.com>
2026-03-03 10:31:21 +0200

    ContactActivity
    Made seven variables in android studio project.

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -10,6 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+val firstName = "Leelah"
+val lastName = "Jaffer"
+val address = "70 Ice Cream Road, Cape Town"
+val isMale = false
+val dayOfBirth = "15"
+val monthOfBirth = "June"
+val yearOfBirth = "2007"        
+
+
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
